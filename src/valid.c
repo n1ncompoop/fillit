dcec1bbbabf0c5e0b2c7f1c639bb378f48b709b6
Roy Kwon <rkwon@e1z2r9p6.42.us.org>
2018-03-21 23:09:44 -0700

    validchange

--- src/valid.c
+++ src/valid.c
@@ -10,43 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int     valid_piece(char **str, int x, int y)
-{
-    int     shrp_cnt;
-
-    shrp_cnt = 0;
-    while (str[y][x])
-    {
-        while (y < 4)
-        {
-            while (x < 4)
-            {
-                if (str[y][x] == '.')
-                    x++;
-                else if (str[y][x] == '#')
-                {
-                    x++;
-                    shrp_cnt++;
-                }   
-                else
-                    return (0);           
-            }
-            if (str[y][x] == '\n')
-            {
-                x = 0;
-                y++;
-            }
-            else
-                return (0);
-        }
-        if (y == 4)
-            if ((shrp_cnt == 4) && valid_shape(str, 0, 0) 
-                && ((str[y][x] == '\n') || (str[y][x] == '\0')))
-                return (1);
-        else
-            return (0);  
-    }
-}
+#include "../libft/libft.h"
+#include "../include/fillit.h"
 
 int     valid_shape(char **str, int x, int y)
 {
@@ -75,4 +40,50 @@ int     valid_shape(char **str, int x, int y)
         return(1);
     else
         return(0);
+}
+
+int     valid_piece(char **str, int x, int y)
+{
+    int     shrp_cnt;
+
+    shrp_cnt = 0;
+    while (str[y][x])
+    {
+        while (y <= 3)
+        {
+            while (x <= 3)
+            {
+                if (str[y][x] == '.')
+                    x++;
+                else if (str[y][x] == '#')
+                {
+                    x++;
+                    shrp_cnt++;
+                }   
+                else
+                    return (0);           
+            }
+            if (str[y][x] == '\n')
+            {
+                x = 0;
+                y++;
+            }
+            else
+                return (0);
+        }
+        if (y == 4)
+            if (((shrp_cnt == 4) && (valid_shape(&str, 0, 0))) 
+                && ((str[y][x] == '\n') || (str[y][x] == '\0')))
+                return (1);
+    }
+    return (0);
+}
+
+int main(int ac, char **av)
+{
+    if (ac == 2)
+    {
+        printf("%d\n", valid_piece(&av[1], 0, 0));
+    }
+    return (0);
 }
